f027377b5590416ff9d1b29a3796a00e70cb5fea
Siyu Chen <syche55@gmail.com>
2019-07-03 22:50:36 -0700

    Create WebDev | Node.js

---
+++ WebDev | Node.js
@@ -0,0 +1,21 @@
+node index.js (在console里打开index.js)
+node
+ctrl + c / clear
+
+const a = 2;  之后不能再给a赋其他值
+
+//jshint esversion:6
+FILE SYSTEM:
+const fs = require("fs"); 
+fs.copyFileSync(src, dest[,flags])
+fs.copyFileSync("file1.txt", "file2.txt");
+
+npm:
+npm init
+npm install superheroes (module)
+在index.js里:
+var superheroes = require("superheroes");
+var mySuperheroName = superheroes.random();
+
+sudo chown -R $USER:$GROUP ~/.npm
+sudo chown -R $USER:$GROUP ~/.config
