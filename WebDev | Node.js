e137b1d7b48de985921f5fe22788fd3db854c6de
Siyu Chen <syche55@gmail.com>
2019-07-08 22:37:05 -0700

    Update WebDev | Node.js

--- WebDev | Node.js
+++ WebDev | Node.js
@@ -12,10 +12,14 @@ fs.copyFileSync("file1.txt", "file2.txt");
 
 npm:
 npm init
+
+
+sudo chown -R $USER:$GROUP ~/.npm
+sudo chown -R $USER:$GROUP ~/.config
+
+sudo npm install express body-parser ejs
+
 npm install superheroes (module)
 在index.js里:
 var superheroes = require("superheroes");
 var mySuperheroName = superheroes.random();
-
-sudo chown -R $USER:$GROUP ~/.npm
-sudo chown -R $USER:$GROUP ~/.config
